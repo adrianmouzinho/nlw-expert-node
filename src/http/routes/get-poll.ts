3e46851d5918235c6580a0f1174d3cc988a8f64b
Willame Mouzinho <willame.dev@gmail.com>
2024-02-10 16:07:44 -0300

    feat: store votes on redis on user vote on poll

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 
 import { prisma } from '../../lib/prisma'
+import { redis } from '../../lib/redis'
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/polls/:pollId', async (request, reply) => {
@@ -11,7 +12,7 @@ export async function getPoll(app: FastifyInstance) {
 
     const { pollId } = getPollParams.parse(request.params)
 
-    const poll = await prisma.poll.findFirstOrThrow({
+    const poll = await prisma.poll.findFirst({
       where: {
         id: pollId,
       },
@@ -25,6 +26,35 @@ export async function getPoll(app: FastifyInstance) {
       },
     })
 
-    return reply.status(200).send(poll)
+    if (!poll) {
+      return reply.status(400).send({ message: 'Poll not found.' })
+    }
+
+    const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES')
+
+    const votes = result.reduce(
+      (obj, item, index) => {
+        if (index % 2 === 0) {
+          const score = result[index + 1]
+
+          Object.assign(obj, { [item]: Number(score) })
+        }
+
+        return obj
+      },
+      {} as Record<string, number>,
+    )
+
+    return reply.status(200).send({
+      id: poll.id,
+      title: poll.title,
+      options: poll.options.map((option) => {
+        return {
+          id: option.id,
+          title: option.title,
+          score: option.id in votes ? votes[option.id] : 0,
+        }
+      }),
+    })
   })
 }
--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -3,6 +3,7 @@ import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
 
 import { prisma } from '../../lib/prisma'
+import { redis } from '../../lib/redis'
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -30,6 +31,7 @@ export async function voteOnPoll(app: FastifyInstance) {
         select: {
           id: true,
           pollOptionId: true,
+          pollId: true,
         },
       })
 
@@ -42,6 +44,8 @@ export async function voteOnPoll(app: FastifyInstance) {
             id: userPreviousVoteOnPoll.id,
           },
         })
+
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
       }
 
       if (
@@ -72,6 +76,8 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollOptionId,
       },
     })
+
+    await redis.zincrby(pollId, 1, pollOptionId)
 
     return reply.status(201).send()
   })
